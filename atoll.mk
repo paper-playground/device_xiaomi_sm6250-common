71232504df5dc2d0152ddda9c2cdf1ddf7856126
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-03-27 17:26:34 +0600

    Revert "sm6250-common: audio: Move `bluetooth_audio_policy_configuration` to DT side"
    This reverts commit f3010d703731238976bf17378fcb183c664c4811.

--- atoll.mk
+++ atoll.mk
@@ -52,12 +52,12 @@ PRODUCT_PACKAGES += \
     audio_platform_info_intcodec.xml \
     audio_policy_configuration.xml \
     audio_policy_volumes.xml \
-    bluetooth_audio_policy_configuration_7_0.xml \
     mixer_paths_wcd937x.xml \
     sound_trigger_mixer_paths.xml \
     sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
